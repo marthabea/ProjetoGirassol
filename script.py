16bf9d96a3d86a09eb142e8bc736fb482cc50b1b
Martha Beatriz <marthinhabeatriz@gmail.com>
2023-10-11 08:07:04 -0300

    Melhoria na estrutura de dados e testes com imagens

--- script.py
+++ script.py
@@ -2,30 +2,30 @@ import cv2
 import pytesseract
 
 # Abrir o arquivo
-img = cv2.imread("")
+image = cv2.imread("teste_letras.png")
 
 # caminho do executável do tesseract
 pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
 
-# configuração para interpretar letras cursivas em português
-
-config_port = r'--oem 3 --psm 6 -l por'
-result_transf_img = pytesseract.image_to_string(img)
-
+# configuração para interpretar letras em português e conversão da img
+config_br = r'--oem 3 --psm 6 -l por'
+new_image = pytesseract.image_to_string(image)
 
 # formatação de string para limpeza do output
-
-def format_text(text=str):
-    lines = text.split('\n')  # Dividir o texto em linhas para melhorar a organização
-    new_text = ""
-    for line in lines: 
+class FormatText:
+    def text(self, text):
+     lines = text.split('\n')  # Dividir o texto em linhas para melhorar a organização
+     new_text = ""
+     for line in lines: 
         line = line.strip()  # Remover espaços em branco no início e no final de cada linha
         if line:  
             new_text += line + '\n'
-    return new_text
 
-# formatação a partir do def
-result = format_text(result_transf_img)
+     return new_text
+
+# formatação do texto contido na minha imagem
+format = FormatText() 
+result = format.text(new_image)
 
 # novo texto com a formatação
 print(result)
